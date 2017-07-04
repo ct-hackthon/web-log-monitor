e6ff6d0562bd24732adf64c4194cacb7787a902e
fulw <fulw@ctrip.com>
2017-07-04 14:37:48 +0800

    init

---
+++ web-log-monitor-service-impl/src/main/java/web.log.monitor.service.impl/Application.java
@@ -0,0 +1,22 @@
+package web.log.monitor.service.impl;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/************************************************************
+ * Copy Right Information : 
+ * Project : ${ProjectName}
+ * JDK version used : ${SDK}
+ * Comments : 
+ *
+ * Modification history : 
+ *
+ * Sr *** Date      *** Modified By *** Why & What is modified
+ * 1. *** 2017/7/4 *** fulw *** Initial
+ ***********************************************************/
+@Configuration
+@ComponentScan
+public class Application {
+
+
+}
