b58bba9d48d7bdad2374625d65dc6fff971eee3e
fulw <fulw@ctrip.com>
2017-07-04 16:59:03 +0800

    init

--- web-log-monitor-dao/src/main/java/web/log/monitor/dao/Application.java
+++ web-log-monitor-dao/src/main/java/web/log/monitor/dao/Application.java
@@ -16,7 +16,7 @@ import javax.sql.DataSource;
  * Modification history : 
  *
  * Sr *** Date      *** Modified By *** Why & What is modified
- * 1. *** 2017/7/4 *** fulw *** Initial
+ * 1. *** 2017/7/4 *** fulw         *** Initial
  ***********************************************************/
 @Configuration
 @ComponentScan
--- web-log-monitor-dao/src/main/java/web/log/monitor/dao/dao/HttpErrorStatisticDao.java
+++ web-log-monitor-dao/src/main/java/web/log/monitor/dao/dao/HttpErrorStatisticDao.java
@@ -11,9 +11,9 @@ import org.springframework.stereotype.Repository;
  * Modification history : 
  *
  * Sr *** Date      *** Modified By *** Why & What is modified
- * 1. *** 2017/7/4 *** fulw         *** Initial
+ * 1. *** 2017/7/4  *** fulw        *** Initial
  ***********************************************************/
-@Repository
+@Repository("httpErrorStatisticDao")
 public class HttpErrorStatisticDao extends BaseDao{
 
 
